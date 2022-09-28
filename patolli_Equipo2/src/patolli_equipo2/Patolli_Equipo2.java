f7d477b2b517e7ceae24b20e6678448bd2a7f3a0
TGroj <114610141+TGroj@users.noreply.github.com>
2022-09-28 11:45:23 -0600

    Add files via upload

---
+++ patolli_Equipo2/src/patolli_equipo2/Patolli_Equipo2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package patolli_equipo2;
+
+import frames.MenuPrincipal;
+
+/**
+ *
+ * @author jonyo
+ */
+public class Patolli_Equipo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        MenuPrincipal menu =new MenuPrincipal();
+        menu.setVisible(true);
+    }
+    
+}
